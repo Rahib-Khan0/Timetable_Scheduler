38125434214adc3f7de76bcdbab28d34e23a39f5
Rahib Khan~ <mohdrahib.2005@example.com>
2025-09-18 14:28:21 +0530

    pycache removed

--- backend_FastAPI/core/jwt.py
+++ backend_FastAPI/core/jwt.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
